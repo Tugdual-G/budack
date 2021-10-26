48041284183117909812090d07744829cebea071
Tugdual-G <tugducarfantan@gmail.com>
2021-10-27 00:05:09 +0200

    tuning

--- budack.c
+++ budack.c
@@ -1,129 +1,132 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "budack.h"
 #include <math.h>
 #include <mpi.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 const int e6 = 1000000;
 const int e3 = 1000;
 
-struct Param
-{
-    long int N;
-    unsigned int nx, ny;
-    unsigned int maxit;
-    unsigned int minit;
-    float a[2];
-    float b[2];
-    unsigned int *B;
-    unsigned int *B_sum;
-    unsigned int *M;
-    double *M_brdr;
+struct Param {
+  long int N;
+  unsigned int nx, ny;
+  unsigned int maxit;
+  unsigned int minit;
+  float a[2];
+  float b[2];
+  unsigned int *B;
+  unsigned int *B_sum;
+  unsigned int *M;
+  double *M_brdr;
 };
 
+int main() {
+  MPI_Init(NULL, NULL); // initialize MPI environment
+  int world_size;       // number of processes
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  int rank; // the rank of the process
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-int main()
-{
-    MPI_Init(NULL, NULL);      // initialize MPI environment
-    int world_size; // number of processes
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    int rank; // the rank of the process
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  ////////////////////////////////////////////////
+  //   Most important parameters
+  ////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////
-    //   Most important parameters
-    ////////////////////////////////////////////////
+  unsigned int nx = 1 * e3; // Grid size x axis
+  unsigned int maxit = 200; // maximum number of iteration per point
+  unsigned int minit = 50;  // minimum iteration per point
+  long int D = 500 * e3;    // density of point, higher = less noise
+  float a[2] = {-2.3, 1.3}, b[2] = {-1.5, 1.5}; // size of the domain a+bi
 
-    unsigned int nx= 1*e3; // Grid size x axis
-    unsigned int maxit = 200; // maximum number of iteration per point
-    unsigned int minit = 20; // minimum iteration per point
-    long int D = 500*e3; // density of point, higher = less noise
-    float a[2]={-2.3, 1.3}, b[2]={-1.5,1.5}; // size of the domain a+bi
+  ///////////////////////////////////////////////
+  ///////////////////////////////////////////////
 
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
+  // x and y are discretized at the midle of the cells
+  double dx = (a[1] - a[0]) / nx;
+  unsigned int ny = 2 * (unsigned int)(b[1] / dx);
+  b[0] = -((int)ny / 2) * dx;
+  b[1] = ((int)ny / 2) * dx;
 
-    // x and y are discretized at the midle of the cells
-    double dx = (a[1]-a[0]) / nx;
-    unsigned int ny = 2*b[1]/dx;
+  if (rank == 0) {
+    printf("\nnx = %d ; ny = %d ; ny*nx= %d \n", nx, ny, ny * nx);
+    printf("maxit = %d ; minit = %d ; density of points %.1e\n", maxit, minit,
+           (double)D);
+    printf("Depth of the data written to disk : %lu \n \n",
+           sizeof(unsigned int));
+    printf("Begin computation on %d cores \n", world_size);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
 
-    if (rank==0){
-        printf("\nnx = %d ; ny = %d ; ny*nx= %d \n", nx, ny, ny*nx);
-        printf("maxit = %d ; minit = %d ; density of points %.1e\n", maxit, minit, (double)D);
-        printf("Depth of the data written to disk : %lu \n \n", sizeof(unsigned int));
-        printf("Begin computation on %d cores \n", world_size);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
+  ////////////////////////////////////////////////
+  //   Searching for points on the boundary
+  ////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////
-    //   Searching for points on the boundary
-    ////////////////////////////////////////////////
+  // There is no need to parallelise this part
 
-    // There is no need to parallelise this part
+  clock_t begin = clock();
 
-    clock_t begin = clock();
+  double *M_brdr;
+  long int Nborder = nx * ny / 20;
+  unsigned int depth = maxit;
 
-    unsigned int *M;
-    double *M_brdr;
-    long int Nborder = nx*ny/20;
-    unsigned int depth = maxit;
-
-
-    M_brdr = (double *)calloc(Nborder, sizeof(double));
-    M = (unsigned int *)calloc((int)nx*ny/2, sizeof(unsigned int));
-    if (M==NULL){printf("Error, no memory space allocated for computing"); return 0;}
-
-    border(a, b[1], depth, Nborder, M, M_brdr);
-
-    clock_t end = clock();
-    float t_comp = (float)(end - begin);
-    t_comp = t_comp/CLOCKS_PER_SEC;
-    if (rank==0)
-        {
-            printf("Core 0 found %ld border points in %f s\n\n", Nborder, t_comp);
-        }
-
-    ////////////////////////////////////////////////
-    //   Cumputing the trajectories
-    ////////////////////////////////////////////////
-
-    srand((unsigned int)time(NULL));
-    unsigned int *B, *B_sum;
-    B = (unsigned int *)calloc(nx*ny, sizeof(unsigned int));
-    B_sum = (unsigned int *)calloc(nx*ny, sizeof(unsigned int));
-    D = D/world_size;
-    if (B_sum==NULL){printf("Error, no memory space (heap) allocated for storing results"); return 0;}
-
-    if (rank==0){begin = clock();}
-
-    trajectories(nx, ny, a, b, B, D, maxit, minit, M_brdr, Nborder);
-
-    MPI_Reduce(B, B_sum, nx*ny, MPI_INT, MPI_SUM, 0,
-        MPI_COMM_WORLD);
-
-    unsigned int arraysize[2]={ny, nx};
-
-    if (rank==0)
-        {
-            end = clock();
-            t_comp = (float)(end - begin);
-            t_comp = t_comp/CLOCKS_PER_SEC;
-            printf("\nTime elapsed computing trajectories %f s \n", t_comp);
-
-            // Storing variables on disk
-            save("trajectories_data/arraysize.uint", arraysize, sizeof(arraysize));
-            save("trajectories_data/boundary.uint", M_brdr, sizeof(double)*Nborder);
-            save_chargrayscale(ny,nx,B_sum,"trajectories_data/b.char");
-        }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    free(M);
-    free(M_brdr);
-    free(B);
-    free(B_sum);
-    if (rank==0){printf("allocated space freed \n");}
-    MPI_Finalize(); // finish MPI environment
+  M_brdr = (double *)calloc(Nborder, sizeof(double));
+  if (M_brdr == NULL) {
+    printf("Error, no memory space allocated for computing");
     return 0;
+  }
+
+  border(depth, Nborder, M_brdr);
+
+  clock_t end = clock();
+  float t_comp = (float)(end - begin);
+  t_comp = t_comp / CLOCKS_PER_SEC;
+  if (rank == 0) {
+    printf("Core 0 found %ld border points in %f s\n\n", Nborder, t_comp);
+  }
+
+  ////////////////////////////////////////////////
+  //   Cumputing the trajectories
+  ////////////////////////////////////////////////
+
+  srand((unsigned int)time(NULL));
+  unsigned int *B, *B_sum;
+  B = (unsigned int *)calloc(nx * ny, sizeof(unsigned int));
+  B_sum = (unsigned int *)calloc(nx * ny, sizeof(unsigned int));
+  D = D / world_size;
+  if (B_sum == NULL) {
+    printf("Error, no memory space (heap) allocated for storing results");
+    return 0;
+  }
+
+  if (rank == 0) {
+    begin = clock();
+  }
+
+  trajectories(nx, ny, a, b, B, D, maxit, minit, M_brdr, Nborder);
+
+  MPI_Reduce(B, B_sum, nx * ny, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  unsigned int arraysize[2] = {ny, nx};
+
+  if (rank == 0) {
+    end = clock();
+    t_comp = (float)(end - begin);
+    t_comp = t_comp / CLOCKS_PER_SEC;
+    printf("\nTime elapsed computing trajectories %f s \n", t_comp);
+
+    // Storing variables on disk
+    save("trajectories_data/arraysize.uint", arraysize, sizeof(arraysize));
+    save("trajectories_data/boundary.uint", M_brdr, sizeof(double) * Nborder);
+    save_chargrayscale(ny, nx, B_sum, "trajectories_data/b.char");
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  free(M_brdr);
+  free(B);
+  free(B_sum);
+  if (rank == 0) {
+    printf("allocated space freed \n");
+  }
+  MPI_Finalize(); // finish MPI environment
+  return 0;
 }
--- budack.h
+++ budack.h
@@ -1,20 +1,19 @@
+#include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-#include <mpi.h>
-
 
 #define PI 3.141592654
-const long int A=9;
+const long int A = 9;
+const char load[4] = "\\|/-";
 
-double gaussrand(double dx)
-{
+double gaussrand(double dx) {
   static double U, V;
   static int phase = 0;
   double Z;
 
-  if(phase == 0) {
+  if (phase == 0) {
     U = (rand() + 1.) / (RAND_MAX + 2.);
     V = rand() / (RAND_MAX + 1.);
     Z = sqrt(-2 * log(U)) * sin(2 * PI * V);
@@ -23,216 +22,179 @@ double gaussrand(double dx)
 
   phase = 1 - phase;
 
-  return Z*dx;
+  return Z * dx;
 }
 
-double randomfloat(double min, double max)
-  {
-    double r, range=max-min;
-    r = (double)rand()/(double)(RAND_MAX);
-    r = min + r * range;
-    return r;
-  }
+double randomfloat(double min, double max) {
+  double r, range = max - min;
+  r = (double)rand() / (double)(RAND_MAX);
+  r = min + r * range;
+  return r;
+}
 
-void trajectories(
-    unsigned int nx,
-    unsigned int ny,
-    float x_b[2],
-    float y_b[2],
-    unsigned int *M_traj,
-    long int D,
-    int maxit,
-    int minit,
-    double *M_brdr,
-    unsigned int Nborder)
-  {
-    // Initialisation
-    int rank; // the rank of the process
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    double dx, x, y, x0, y0, x2, y2;
+void trajectories(unsigned int nx, unsigned int ny, float x_b[2], float y_b[2],
+                  unsigned int *M_traj, long int D, int maxit, int minit,
+                  double *M_brdr, unsigned int Nborder) {
+  // Initialisation
+  int rank; // the rank of the process
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  double dx, x, y, x0, y0, x2, y2;
 
-    // ntraj is the nuber of trajectories.
-    // it is the number of iteration for a trajectory.
-    long int ntraj = 0, it = 0, itraj=0;
-    unsigned int ij[maxit*2+1], i;
-    char diverge=0;
-    long int mean_it_per_point=0;
+  // ntraj is the nuber of trajectories.
+  // it is the number of iteration for a trajectory.
+  long int ntraj = 0, it = 0, itraj = 0;
+  unsigned int ij[maxit * 2 + 1], i;
+  char diverge = 0;
+  long int current_D = 0;
 
-    dx = (x_b[1]-x_b[0]) / nx;
-    while (((long int)ntraj/A) < (long int)D )
-      {
-        y0 = M_brdr[(2*itraj)%Nborder]+gaussrand(5*dx);
-        x0 = M_brdr[(2*itraj+1)%Nborder]+gaussrand(5*dx);
+  dx = (x_b[1] - x_b[0]) / nx;
+  while (current_D < D) {
+    y0 = M_brdr[(2 * itraj) % Nborder] + gaussrand(5 * dx);
+    x0 = M_brdr[(2 * itraj + 1) % Nborder] + gaussrand(5 * dx);
 
-        it = 0;
-        x = x0;
-        y = y0;
-        x2 = x*x;
-        y2 = y*y;
-        diverge = 0;
+    it = 0;
+    x = x0;
+    y = y0;
+    x2 = x * x;
+    y2 = y * y;
+    diverge = 0;
 
-        /* // Here we test if the point is obviously in the set */
-        /* // i.e. main cardioid and disk */
-        /* q = (x-1/4)*(x-1/4)+y2; */
-        /* if (q*(q+(x-1/4))<y2/5 || (x+1)*(x+1)+y2<1/17){ */
-        /*   // If the point is in the set we do not iterate */
-        /*   // and we search for another */
-        /*   it = maxit; */
-        /* } */
-        ij[it*2] = (y-y_b[0])/dx;
-        ij[it*2+1] = (x-x_b[0])/dx;
+    /* // Here we test if the point is obviously in the set */
+    /* // i.e. main cardioid and disk */
+    /* q = (x-1/4)*(x-1/4)+y2; */
+    /* if (q*(q+(x-1/4))<y2/5 || (x+1)*(x+1)+y2<1/17){ */
+    /*   // If the point is in the set we do not iterate */
+    /*   // and we search for another */
+    /*   it = maxit; */
+    /* } */
+    ij[it * 2] = (y - y_b[0]) / dx;
+    ij[it * 2 + 1] = (x - x_b[0]) / dx;
 
-
-        while ( it < maxit && diverge==0)
-          {
-            // Storing the trajectories
-            y = 2 * x * y + y0;
-            x = x2 - y2 + x0;
-            x2 = x*x;
-            y2 = y*y;
-            it++;
-            if (x_b[0] < x && x < x_b[1] && y_b[0] < y && y < y_b[1])
-              {
-                ij[it*2] = (y-y_b[0])/dx;
-                ij[it*2+1] = (x-x_b[0])/dx;
-              }
-            if (x2+y2>16)
-              {
-                diverge = 1;
-              }
-          }
-        if (diverge == 1 && it>minit)
-          {
-            ntraj++;
-            mean_it_per_point += it;
-            for ( i=0; i<it; i+=2 )
-              {
-                *(M_traj+nx*ij[i]+ij[i+1]) += 1;
-              }
-            if (rank==0 && ntraj % 100000 == 0)
-              {
-                printf("\rDensity of point %-5ld/%5ld (core 0)", (long int) ntraj/(100*A), D/100 );
-                fflush(stdout);
-              }
-          }
-        itraj++;
+    while (it < maxit && diverge == 0) {
+      // Storing the trajectories
+      y = 2 * x * y + y0;
+      x = x2 - y2 + x0;
+      x2 = x * x;
+      y2 = y * y;
+      it++;
+      if (x_b[0] < x && x < x_b[1] && y_b[0] < y && y < y_b[1]) {
+        ij[it * 2] = (y - y_b[0]) / dx;
+        ij[it * 2 + 1] = (x - x_b[0]) / dx;
       }
-  }
-
-
-void border(float x_b[2],
-            float y_b,
-            unsigned int depth,
-            long int Npts,
-            unsigned int *M,
-            double *M_brdr)
-  {
-    // Return the list of the points at the boundary in index coordinates
-    // relative to the subdomain bodaries.
-    double x, y, x0, y0, x2, y2;
-    unsigned int it=0, start=500;
-    unsigned int k=0, n=0;
-    unsigned char mindepth = depth*0.9;
-    float sigma=50/start;
-
-    while (2*k<Npts)
-      {
-        it = 0;
-        if (k<start)
-          {
-            x0 = randomfloat(-2, 1);
-            y0 = randomfloat(-1.5, 1.5);
-          }
-        else
-          {
-            n = n%k;
-            x0 = *(M_brdr+n*2+1)+gaussrand(sigma);
-            y0 = *(M_brdr+n*2)+gaussrand(sigma);
-            n+=20;
-          }
-        x = x0;
-        y = y0;
-        x2 = x*x;
-        y2 = y*y;
-
-        while ( it < depth && x2+y2 < 4)
-          {
-            // Storing the trajectories
-            y = 2 * x * y + y0;
-            x = x2 - y2 + x0;
-            x2 = x*x;
-            y2 = y*y;
-            it++;
-          }
-        if (it < depth && it >= mindepth)
-          {
-            *(M_brdr+k*2)= y0;
-            *(M_brdr+k*2+1) = x0;
-            k++;
-          }
+      if (x2 + y2 > 4) {
+        diverge = 1;
       }
-  }
-
-void save(char fname[], void *data, unsigned int size)
-  {
-    FILE *fp;
-    fp = fopen(fname, "w+");
-    if(fp == NULL)
-    {
-        printf("Error opening file %s, cannot save.\n", fname);
-        exit(1);
     }
-    fwrite(data, size, 1, fp);
-    fclose(fp);
+    if (diverge == 1 && it > minit) {
+      for (i = 0; i < it; i += 2) {
+        *(M_traj + nx * ij[i] + ij[i + 1]) += 1;
+      }
+      if (rank == 0 && ntraj % 1000 == 0) {
+        printf("\rDensity of point %-6ld/%6ld (core 0)", current_D, D);
+        fflush(stdout);
+      }
+      ntraj++;
+      current_D = ntraj / A;
+    }
+    itraj++;
+  }
+  if (rank == 0) {
+    printf("\rDensity of point %-6ld/%6ld (core 0)", current_D, D);
+    fflush(stdout);
+  }
+}
+
+void border(unsigned int depth, long int Npts, double *M_brdr) {
+  // Return the list of the points at the boundary in index coordinates
+  // relative to the subdomain bodaries.
+  double x, y, x0, y0, x2, y2;
+  unsigned int it = 0, start = 500;
+  unsigned int k = 0, n = 0;
+  unsigned char mindepth = depth * 0.9;
+  float sigma = 50 / start;
+
+  while (2 * k < Npts) {
+    it = 0;
+    if (k < start) {
+      x0 = randomfloat(-2, 1);
+      y0 = randomfloat(-1.5, 1.5);
+    } else {
+      n = n % k;
+      x0 = *(M_brdr + n * 2 + 1) + gaussrand(sigma);
+      y0 = *(M_brdr + n * 2) + gaussrand(sigma);
+      n += 20;
+    }
+    x = x0;
+    y = y0;
+    x2 = x * x;
+    y2 = y * y;
+
+    while (it < depth && x2 + y2 < 4) {
+      // Storing the trajectories
+      y = 2 * x * y + y0;
+      x = x2 - y2 + x0;
+      x2 = x * x;
+      y2 = y * y;
+      it++;
+    }
+    if (it < depth && it >= mindepth) {
+      *(M_brdr + k * 2) = y0;
+      *(M_brdr + k * 2 + 1) = x0;
+      k++;
+    }
+  }
+}
+
+void save(char fname[], void *data, unsigned int size) {
+  FILE *fp;
+  fp = fopen(fname, "w+");
+  if (fp == NULL) {
+    printf("Error opening file %s, cannot save.\n", fname);
+    exit(1);
+  }
+  fwrite(data, size, 1, fp);
+  fclose(fp);
+}
+
+void mirror_traj(unsigned int ny, unsigned int nx, unsigned int *B) {
+  unsigned long k;
+  unsigned int i, j;
+  unsigned int b;
+
+  for (i = 0; i < ny; i++) {
+    for (j = 0; j < nx; j++) {
+      k = (unsigned long)ny * nx - (1 + i) * nx + j;
+      b = B[i * nx + j];
+      B[i * nx + j] += B[k];
+      B[k] += b;
+    }
+  }
+}
+
+void save_chargrayscale(unsigned int ny, unsigned int nx, unsigned int *B,
+                        char fname[]) {
+  unsigned long size = nx * ny;
+  unsigned char *B_c;
+  B_c = (unsigned char *)malloc(sizeof(unsigned char) * size);
+
+  unsigned long k;
+  unsigned int bmax = 0;
+
+  mirror_traj(ny, nx, B);
+
+  for (k = 0; k < size; k++) {
+    if (*(B + k) > bmax) {
+      bmax = *(B + k);
+    }
+  }
+  printf("Maximum accumulted points %u \n", bmax);
+  for (k = 0; k < size; k++) {
+    *(B_c + k) = (double)*(B + k) * 255 / bmax;
   }
 
-void mirror_traj(unsigned int ny, unsigned int nx, unsigned int *B)
-  {
-    unsigned long k;
-    unsigned int i, j;
-    unsigned int b;
-
-    for (i=0;i<ny;i++)
-        {
-          for(j=0;j<nx;j++)
-            {
-              k = (unsigned long) ny*nx-(1+i)*nx+j;
-              b = B[i*nx+j];
-              B[i*nx+j] += B[k];
-              B[k] += b;
-            }
-
-        }
-  }
-
-void save_chargrayscale(unsigned int ny, unsigned int nx, unsigned int *B, char fname[])
-  {
-    unsigned long size = nx*ny;
-    unsigned char *B_c;
-    B_c = (unsigned char *) malloc(sizeof(unsigned char)*size);
-
-    unsigned long k;
-    unsigned int bmax=0;
-
-    mirror_traj(ny,nx,B);
-
-    for (k=0; k<size; k++)
-        {
-            if (*(B+k)>bmax)
-                {
-                    bmax = *(B+k);
-                }
-        }
-    printf("Maximum accumulted points %u \n", bmax );
-    for (k=0; k<size; k++)
-        {
-            *(B_c+k) = (double) *(B+k) * 255/bmax ;
-        }
-
-
-    FILE *fptr;
-    fptr = fopen(fname, "w+");
-    fwrite(B_c, sizeof(unsigned char), size, fptr);
-    fclose(fptr);
-    free(B_c);
-  }
+  FILE *fptr;
+  fptr = fopen(fname, "w+");
+  fwrite(B_c, sizeof(unsigned char), size, fptr);
+  fclose(fptr);
+  free(B_c);
+}
