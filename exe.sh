096db7fd7a55e13085aa074848c60bff5eee21cb
Tugdual-G <tugducarfantan@gmail.com>
2021-11-01 15:08:55 +0100

    pineananas

--- exe.sh
+++ exe.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 budack=core/budack
 trajdir0=output/traj0/
-trajdir=output/images0/
-nx=8000
+trajdir=output/images/
+nx=1000
 density=8
 
 # Image sizes (nx x ny):
@@ -10,8 +10,9 @@ if [ $nx = 1000 ];then ny=832;fi
 if [ $nx = 2000 ];then ny=1666;fi
 if [ $nx = 4000 ];then ny=3332;fi
 if [ $nx = 8000 ];then ny=6666;fi
-#------------- computing  -------------
-mpiexec --mca opal_warn_on_missing_libcuda 0 $budack $nx 10000 100 $density
+
+# #------------- computing  -------------
+mpiexec --mca opal_warn_on_missing_libcuda 0 $budack $nx 200 0 $density
 rm ${trajdir}/*
 mv ${trajdir0}/* $trajdir
 
@@ -27,11 +28,11 @@ magick convert -size ${nx}x${ny} -depth 8 \
 echo --------- Creating colored image --------
 magick convert -size ${nx}x${ny} -depth 8 \
     gray:${trajdir}traj0.char gray:${trajdir}traj1.char gray:${trajdir}traj2.char \
-    -channel RGB -combine -gamma 1 -rotate 90 ${trajdir}rgb0.png
+    -channel RGB -combine -gamma 1.2 -rotate 90 ${trajdir}rgb0.png
 
 magick convert -size ${nx}x${ny} -depth 8 \
     gray:${trajdir}traj0.char gray:${trajdir}traj2.char gray:${trajdir}traj1.char \
-    -channel RGB -combine -gamma 1 -rotate 90 ${trajdir}rgb1.png
+    -channel RGB -combine -gamma 1.3 -rotate 90 ${trajdir}rgb1.png
 
 magick convert -size ${nx}x${ny} -depth 8 \
     gray:${trajdir}traj1.char gray:${trajdir}traj0.char gray:${trajdir}traj2.char \
