b002eefd2fa01acbf9198d3675212090e7053bfc
Tugdual Gaillard <77338231+Tugdual-G@users.noreply.github.com>
2021-11-11 22:30:44 +0100

    Update gamma.sh

--- gamma.sh
+++ gamma.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script aplly gamma transform to enhance the images
+# This script apply gamma transform to enhance the images
 
 # Location of the executable, you shouldn't change this.
 scrpt_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
